c9b1fdcbab342d69dbd4349c3f8f8fe7373a7473
Gjc452 <1796444035@qq.com>
2021-01-17 21:05:08 +0800

    ‘props 钩子测试’

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './style.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: 1
+    }
+  }
+
+  add = () => {
+    this.setState({
+      x: this.state.x + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        App
+        <button onClick={this.add}>+1</button>
+        <B name={this.state.x}/>
+      </div>
+    )
+  }
+}
+
+class B extends React.Component {
+  componentWillReceiveProps(nextProps, nextContext) {
+    console.log('旧的 props')
+    console.log(this.props)
+    console.log('props变化了')
+    console.log('新的 props')
+    console.log(nextProps)
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+}
+
+
+const rootElement = document.querySelector('#root')
+ReactDOM.render(<App/>, rootElement)
